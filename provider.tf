30f97e5fc6de158c939711a0f15f809e8c6d9d75
bajpai30amit <98395683+bajpai30amit@users.noreply.github.com>
2022-11-29 17:46:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -23,9 +23,3 @@ provider "google" {
     project = var.gcp_project_id
     region = var.gcp_region
 }
-terraform {
-  backend "gcs" {
-    bucket  = "terrafomr-test-amit-win"
-    prefix  = "terraform-state"
-  }
-}
